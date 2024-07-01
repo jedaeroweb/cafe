c06c3264e84f626a6f4dcc82208b4757947e8846
잠자는사자(Mac) <admin@sleepinglion.pe.kr>
2024-07-01 22:22:44 +0900

    modified:   Gemfile.lock 	modified:   config/deploy.rb 	modified:   config/deploy/production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:jedaeroweb/cafe.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/cafe"
+set :deploy_to, "/var/www/html/cafe"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
