e92cdaaad92236967160371aea8b00a511dbe314
sleepinglion <admin@jedaeroweb.co.kr>
2026-04-27 15:45:55 +0900

    수정함:        app/javascript/channels/index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,7 +1,4 @@
 // Import all the channels to be used by Action Cable
-<<<<<<< HEAD
 import "./consumer"
 import "./admin/orders_channel"
-=======
 import "channels/admin/orders_channel"
->>>>>>> 81abcb6e2ef63f862089538376c17f9116fd2e68
